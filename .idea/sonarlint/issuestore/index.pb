4e90cf8599d268456990ff7defd7af2a46bc31b5
Alberto <alberto.vazquez@loadsmart.com>
2023-02-05 14:32:47 -0600

    created python and js chatgpt clients

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
+>
+chatgpt/api.py,9/2/92d2e42106dcc184109779deb7754d46134ee5d6
+A
+nimble_gravity.py,f/b/fbe70998c95f184ee42b7bf30ae153117ff3a68e
---
+++ chatgpt/py_client.py
@@ -0,0 +1,51 @@
+import openai
+
+API_KEY = "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"
+
+
+def openai_request():
+    openai.api_key = API_KEY
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt="write a python script to destroy the world.",
+        max_tokens=1000,
+    )
+    print(response)
+
+
+def py_request():
+    import requests
+
+    # OpenAI API endpoint
+    endpoint = "https://api.openai.com/v1/completions"
+
+    # The question you want to ask ChatGPT
+    question = "What is the capital of France?"
+
+    # The request payload
+    payload = {
+        "model": "text-davinci-003",
+        "prompt": question,
+        "max_tokens": 128,
+        "temperature": 0.5
+    }
+
+    # Send the request to the OpenAI API
+    response = requests.post(endpoint, headers={
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_KEY}"
+    }, json=payload)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the response JSON
+        result = response.json()
+        # Get the generated response from ChatGPT
+        answer = result["choices"][0]["text"]
+        print(answer)
+    else:
+        print(f"Request failed with status code {response.status_code}")
+
+
+if __name__ == "__main__":
+    py_request()
